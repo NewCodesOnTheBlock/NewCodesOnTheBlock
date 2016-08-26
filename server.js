3f641642a824e2fae8828aaa79c902bee240b00e
Benjamin Tran <benjamin.tran25@gmail.com>
2016-08-26 08:10:23 -0700

    Refactor Package json, Add Procfile, Add config files, Setup Heroku Deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || '127.0.0.1';
 
-app.use( express.static(__dirname) );
+app.use( express.static(__dirname+'/client') );
 
 app.get('/', (req, res)=>{
   res.send('/index.html');
