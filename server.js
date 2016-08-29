a7f248bc7eda7b20d758024164d69ffdb286720d
Battenfield <chrisbattenfield@gmail.com>
2016-08-29 13:00:41 -0700

    Change to 50 results per page and remove comment

--- server.js
+++ server.js
@@ -16,32 +16,25 @@ app.use(requestIp.mw());
 
 app.get('/ip', (req, res)=>{
   var clientIp = req.clientIp;
-  request('https://api.seatgeek.com/2/events?taxonomies.name=concert&geoip='+clientIp+'&range=12mi', function(error,response,body){
+  request('https://api.seatgeek.com/2/events?taxonomies.name=concert&geoip='+clientIp+'&range=12mi&per_page=50', function(error,response,body){
     if (error) console.error(error);
     res.send(body);
   });
 });
 
-// app.post('/events',(req,res)=>{
-//    //API call to events
-//    request('https://api.seatgeek.com/2/performers/266',(request,response)=>{
-//          res.send(response);
-//    });
-// });
 app.get('/artist',(req,res)=>{
 	//get artist info from req.body
 	//API call to spotify to get artist playlist id
 	//send back src for front-end <iframe> tag
     res.send('https://embed.spotify.com/?uri=spotify:user:spotify:playlist:1yHZ5C3penaxRdWR7LRIOb'); //playlist ID: 6rqhFgbbKwnb9MLmUQDhG6
 });
+
 app.get('/book',(req,res)=>{
     //get artist/event info from req.body
     //API call to seatgeek for specific event url
     //redirect to specific url
     res.redirect('https://seatgeek.com/');//for simplicity, redirect to seatgeek for now
 });
-
-
 
 app.set('port', port);
 
