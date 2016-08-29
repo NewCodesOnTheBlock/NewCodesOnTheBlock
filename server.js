9a4e6ddf430e19279191cec12bf8285efed1afcb
phoebe <yeyuting18@gmail.com>
2016-08-29 15:54:27 -0700

    Complete artist request from front-end and response back uri for SPOTIFY player

--- server.js
+++ server.js
@@ -28,11 +28,32 @@ app.get('/ip', (req, res)=>{
 //          res.send(response);
 //    });
 // });
-app.get('/artist',(req,res)=>{
-	//get artist info from req.body
-	//API call to spotify to get artist playlist id
-	//send back src for front-end <iframe> tag
-    res.send('https://embed.spotify.com/?uri=spotify:user:spotify:playlist:1yHZ5C3penaxRdWR7LRIOb'); //playlist ID: 6rqhFgbbKwnb9MLmUQDhG6
+app.get('/artist', (req, res)=> {
+  //get artist_name and genre from req.body
+  //send API call to spotify to ask for artist/genre
+  let artist = req.body.artist;
+  let genre = req.body.genre;
+  artist = artist.split(' ').join('+');
+  genre = genre;
+  let input = artist;
+  let url = `https://api.spotify.com/v1/search?q=${artist}&type=artist`;    
+  //send http request for artist: 
+  request(url, (request, response) => {
+    //if artist exist in spotify
+    if (response.artists.items.length > 1) {
+      let id = response.artists.uri;
+      let link = `https://embed.spotify.com/?uri=${id}`;
+      res.send(link); //send back src for front-end <iframe> tag
+    } else { //if artist NOT exist in spotify
+      //send http request for genre
+      let input = genre;
+      request(url, (request, response) => {
+        let id = response.artists.uri;
+        let link = `https://embed.spotify.com/?uri=${id}`;
+        res.send(link); //send back src for front-end <iframe> tag
+      });
+    }
+  });
 });
 app.get('/book',(req,res)=>{
     //get artist/event info from req.body
