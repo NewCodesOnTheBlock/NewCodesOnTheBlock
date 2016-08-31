9ad614024f8a090670af2443fba7432bfcc06782
phoebe <yeyuting18@gmail.com>
2016-08-31 12:49:18 -0700

    Develop child-view for spotify player

--- client/events/events.player.js
+++ client/events/events.player.js
@@ -1,4 +1,14 @@
 angular.module('events.player', [])
-.controller('PlayerCtrl', ["$scope", "$stateParams", "Events", function($scope, $stateParams, Events) {
-  $scope.events = $stateParams.events;
+.controller('PlayerCtrl', ["$scope", "$stateParams", "$sce","Events", function($scope, $stateParams, $sce,Events) {
+  $scope.player = $stateParams.play;
+  let artist = $scope.player;
+  //console.log(artist,"artist")
+  Events.getArtist(artist)
+  .then((data) => {
+    $scope.data = $sce.trustAsResourceUrl(data);
+  })
+  .catch((error) =>{
+    console.log(error);
+  });
+  
 }]);
