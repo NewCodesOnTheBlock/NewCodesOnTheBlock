60d2506025416770ebc39f161c52c94f3645756d
Battenfield <chrisbattenfield@gmail.com>
2016-09-01 15:44:10 -0700

    Refactor frontend to allow users to search by zipcode, needs to be cleaned up

--- client/auth/login.js
+++ client/auth/login.js
@@ -1,7 +1,8 @@
 angular.module('login', [])
 
-.controller('LoginCtrl', ["$scope", function ($scope) {
-  $scope.signIn = function(){
+.controller('LoginCtrl', ["$scope", "Events", "$http", "$state",
+  function ($scope, Events, $http, $state) {
+    $scope.signIn = function(){
       console.log('inside signIn');
       $http({
         method: 'GET',
@@ -10,4 +11,33 @@ angular.module('login', [])
         return resp.data;
       });
     };
-}]);
+    $scope.postalSearch = function() {
+
+      if ($scope.zipcode && $scope.zipcode.length > 4) {
+        // search zip
+        Events.setZip($scope.zipcode);
+        Events.findZip($scope.zipcode)
+          .then(function(data) {
+            console.log('data: ', data);
+            Events.setListData(data.data.events);
+            $state.go('events');
+          });
+
+      } else {
+        // search IP
+        console.log('else');
+      }
+
+
+
+      // $http({
+      //   data: {zip: $scope.zipcode},
+      //   method:'POST',
+      //   url: '/zip'
+      // }).then(function (resp){
+      //   // go to event
+      //   $state.go('events');
+      //   return resp.data;
+      // });
+    };
+  }]);
--- client/events/events.js
+++ client/events/events.js
@@ -2,31 +2,46 @@ angular.module('events.list', [])
 
 .controller('EventsCtrl', ["$scope", "Events", function($scope, Events) {
   $scope.list = [];
+
+  $scope.list = Events.getEventList();
+
+
   $scope.findEvents = () => {
     Events.findEvents()
       .then((concert) => {
-        const eventList = concert.data.events;
-        eventList.forEach(function(event) {
-          $scope.list.push(event);
-        });
+        // const eventList = concert.data.events;
+        // eventList.forEach(function(event) {
+        //   $scope.list.push(event);
+        // });
+
+        console.log('concert: ', concert);
+        $scope.list = concert.data.events;
       })
       .catch((error) => {
         console.error(error);
       });
   };
-  $scope.findEvents();
-  // $scope.getArtist = () => {
-  //   Events.getArtist(artist)
-  //  .then((data) => {
-  //  //console.log(data, 'data from events.js');
-  //  //   let myEl = angular.element( document.querySelector( '#player' ) );
-  //  //   myEl.html('<iframe src = ' + data + ' width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');
-  //  console.log(data);
-  //  $scope.data = data;
-  //  })
-  //  .catch((error) => {
-  //    console.error(error);
-  //  });
-  //};
+
+  if (!$scope.list) {
+    $scope.findEvents();
+    $scope.list = Events.getEventList();
+  }
+
+  console.log('$scope.list: ', $scope.list);
+
+  // $scope.findZip = () => {
+  //   Events.findZip($scope.zipcode)
+  //     .then((concert) => {
+  //       const eventList = concert.data.events;
+  //       eventList.forEach(function(event) {
+  //         $scope.list.push(event);
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
+  // if(Events.)
+    // $scope.findEvents();
 
 }]);
