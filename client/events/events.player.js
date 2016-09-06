5b5c40b7b13bbe37384645910f44657c95899b37
phoebe <yeyuting18@gmail.com>
2016-09-06 14:17:57 -0700

    Implement google map on the website

--- client/events/events.player.js
+++ client/events/events.player.js
@@ -1,10 +1,22 @@
 angular.module('events.player', [])
 .controller('PlayerCtrl', ["$scope", "$stateParams", "$sce","Events", function($scope, $stateParams, $sce,Events) {
-  $scope.player = $stateParams.play;
-  let artist = $scope.player;
+  $scope.input = $stateParams.play;
+  let input = $scope.input.split('+');
+  let artist = input[0];
+  let venue = input[1];
+  let city = input[2];
+  console.log(artist,"-----", venue, "------", city);
+  //console.log(artist,"artist-------------");
   Events.getArtist(artist)
   .then((data) => {
-    $scope.data = $sce.trustAsResourceUrl(data);
+    $scope.player = $sce.trustAsResourceUrl(data);
+  })
+  .catch((error) =>{
+    console.log(error);
+  });
+  Events.getMap(venue, city)
+  .then((data) => {
+    $scope.map = $sce.trustAsResourceUrl(data);
   })
   .catch((error) =>{
     console.log(error);
